6897589d95f1c5e976d3587d8b1dda6c18712435
Erik Porsche <porsche@mikroskop-center.de>
2022-09-22 14:53:10 +0200

    missed a creat function

--- app/code/community/BL/CustomGrid/Object.php
+++ app/code/community/BL/CustomGrid/Object.php
@@ -17,14 +17,14 @@ class BL_CustomGrid_Object extends Varien_Object
 {
     /**
      * Keys of the objects that should be cloned when the containing object is
-     * 
+     *
      * @var array
      */
     protected $_clonableObjectsKeys = array();
-    
+
     /**
      * Clone the necessary contained objects
-     * 
+     *
      * @param array $data Main data array, or a subset
      * @param string $key Key of the given data array
      */
@@ -32,7 +32,7 @@ class BL_CustomGrid_Object extends Varien_Object
     {
         foreach ($data as $subKey => $value) {
             $valueKey = ($key !== '' ? $key . '/' . $subKey : $subKey);
-            
+
             if (is_array($value)) {
                 $this->_cloneObjects($data[$subKey], $valueKey);
             } elseif (in_array($valueKey, $this->_clonableObjectsKeys, true)) {
@@ -40,25 +40,25 @@ class BL_CustomGrid_Object extends Varien_Object
             }
         }
     }
-    
+
     public function __clone()
     {
         $this->_cloneObjects($this->_data);
     }
-    
+
     /**
      * Return the keys of the objects that should be cloned when the containing object is
-     * 
+     *
      * @return array
      */
     public function getClonableObjectsKeys()
     {
         return $this->_clonableObjectsKeys;
     }
-    
+
     /**
      * Set the keys of the objects that should be cloned when the containing object is
-     * 
+     *
      * @param array $keys Data keys
      * @return BL_CustomGrid_Object
      */
@@ -67,10 +67,10 @@ class BL_CustomGrid_Object extends Varien_Object
         $this->_clonableObjectsKeys = array_unique($keys);
         return $this;
     }
-    
+
     /**
      * Add keys of objects that should be cloned when the containing object is
-     * 
+     *
      * @param array $keys Data kets
      * @return BL_CustomGrid_Object
      */
@@ -79,10 +79,10 @@ class BL_CustomGrid_Object extends Varien_Object
         $this->_clonableObjectsKeys = array_unique(array_merge($this->_clonableObjectsKeys, $keys));
         return $this;
     }
-    
+
     /**
      * Merge the given data into the original data
-     * 
+     *
      * @param array $data Data to merge
      * @param bool $recursive Whether the merging should be recursive
      * @return BL_CustomGrid_Object
@@ -96,12 +96,12 @@ class BL_CustomGrid_Object extends Varien_Object
         }
         return $this;
     }
-    
+
     /**
      * Append the given value or array to the given data key
      * If the corresponding data value is not an array, then it will be wrapped into an array before the appending
      * Note that if the appended value is an array, its keys will be lost
-     * 
+     *
      * @param string $key Data key
      * @param mixed $value Value or array to append
      * @return BL_CustomGrid_Object
@@ -119,13 +119,13 @@ class BL_CustomGrid_Object extends Varien_Object
         $this->setData($key, $data);
         return $this;
     }
-    
+
     /**
      * Substract the given value from the given data key
      * If the corresponding data value is an array, all the sub values that it contains which are (strictly) equal
      * to the given value will be unset
      * Otherwise, the data key will be unset if the data value is (strictly) equal to the given value
-     * 
+     *
      * @param string $key Data key
      * @param mixed $value Substracted value
      * @param bool $strict Whether the equality test should be strict
@@ -136,12 +136,12 @@ class BL_CustomGrid_Object extends Varien_Object
         if ($this->hasData($key)) {
             if (is_array($data = $this->getData($key))) {
                 $subKeys = array_keys($data, $value, $strict);
-                
+
                 if (!empty($subKeys)) {
                     foreach ($subKeys as $subKey) {
                         unset($data[$subKey]);
                     }
-                    
+
                     $this->setData($key, $data);
                 }
             } else {
@@ -152,10 +152,10 @@ class BL_CustomGrid_Object extends Varien_Object
         }
         return $this;
     }
-    
+
     /**
      * Compare the values corresponding to the given data key as-is between this object and the given one
-     * 
+     *
      * @param string $key Data key
      * @param BL_CustomGrid_Object $object Object with which to compare the given value
      * @return int 1 if the value from this object is greater, -1 if it is lesser, 0 if both values are equal
@@ -166,10 +166,10 @@ class BL_CustomGrid_Object extends Varien_Object
         $otherValue = $object->getData($key);
         return ($value > $otherValue ? 1: ($value < $otherValue ? -1 : 0));
     }
-    
+
     /**
      * Compare the values corresponding to the given data key as integers between this object and the given one
-     * 
+     *
      * @param string $key Data key
      * @param BL_CustomGrid_Object $object Object with which to compare the given value
      * @return int 1 if the value from this object is greater, -1 if it is lesser, 0 if both values are equal
@@ -180,10 +180,10 @@ class BL_CustomGrid_Object extends Varien_Object
         $otherValue = (int) $object->getData($key);
         return ($value > $otherValue ? 1: ($value < $otherValue ? -1 : 0));
     }
-    
+
     /**
      * Compare the values corresponding to the given data key as strings between this object and the given one
-     * 
+     *
      * @param string $key Data key
      * @param BL_CustomGrid_Object $object Object with which to compare the given value
      * @return int 1 if the value from this object is greater, -1 if it is lesser, 0 if both values are equal
@@ -194,11 +194,11 @@ class BL_CustomGrid_Object extends Varien_Object
         $otherValue = (string) $object->getData($key);
         return ($withCase ? strcmp($value, $otherValue) : strcasecmp($value, $otherValue));
     }
-    
+
     /**
      * Sort the values contained in the array corresponding to the given data key
      * (does nothing if the value is not an array)
-     * 
+     *
      * @see sort()
      * @param string $key Data key
      * @param int $flags Sorting type flags
@@ -212,11 +212,11 @@ class BL_CustomGrid_Object extends Varien_Object
         }
         return $this;
     }
-    
+
     /**
      * Sort by key the values contained in the array corresponding to the given data key
      * (does nothing if the value is not an array)
-     * 
+     *
      * @see ksort()
      * @param string $key Data key
      * @param int $flags Sorting type flags
@@ -230,21 +230,21 @@ class BL_CustomGrid_Object extends Varien_Object
         }
         return $this;
     }
-    
+
     /**
      * Explode the given compound data key, return only the valid sub keys
-     * 
+     *
      * @param string $key Compound data key
      * @return array
      */
     protected function _explodeCompoundKey($key)
     {
-        return array_filter(explode('/', $key), create_function('$v', 'return ($v !== "");'));
+        return array_filter(explode('/', $key), function($v) {return ($v !== "");});
     }
-    
+
     /**
      * Set the value corresponding to the given data key (support compound keys)
-     * 
+     *
      * @param string $key Data key (can be compound)
      * @param mixed $value Data value
      * @return BL_CustomGrid_Object
@@ -255,7 +255,7 @@ class BL_CustomGrid_Object extends Varien_Object
             $this->_hasDataChanges = true;
             $data =& $this->_data;
             $keys =  $this->_explodeCompoundKey($key);
-            
+
             foreach ($keys as $key) {
                 if (!isset($data[$key])
                     || !(is_array($data[$key]) || ($data[$key] instanceof Varien_Object))) {
@@ -267,16 +267,16 @@ class BL_CustomGrid_Object extends Varien_Object
                     $data =& $data[$key];
                 }
             }
-            
+
             $data = $value;
             return $this;
         }
         return parent::setData($key, $value);
     }
-    
+
     /**
      * Unset the value corresponding to the given data key (support compound keys)
-     * 
+     *
      * @param string $key Data key (can be compound)
      * @return BL_CustomGrid_Object
      */
@@ -288,7 +288,7 @@ class BL_CustomGrid_Object extends Varien_Object
             $keys    =  $this->_explodeCompoundKey($key);
             $lastKey =  array_pop($keys);
             $found   =  true;
-            
+
             foreach ($keys as $key) {
                 if (isset($data[$key])
                     && (is_array($data[$key]) || ($data[$key] instanceof Varien_Object))) {
@@ -302,20 +302,20 @@ class BL_CustomGrid_Object extends Varien_Object
                     break;
                 }
             }
-            
+
             if ($found) {
                 unset($data[$lastKey]);
             }
-            
+
             return $this;
         }
         return parent::unsetData($key);
     }
-    
+
     /**
      * Return the value corresponding to the given data key, apply the given default value first if no value is set
      * (support compound keys)
-     * 
+     *
      * @param string $key Data key
      * @param mixed $default Default value
      * @return mixed
@@ -327,7 +327,7 @@ class BL_CustomGrid_Object extends Varien_Object
             $keys    =  $this->_explodeCompoundKey($key);
             $lastKey =  array_pop($keys);
             $found   =  true;
-            
+
             foreach ($keys as $key) {
                 if (!isset($data[$key])
                     || !(is_array($data[$key]) || ($data[$key] instanceof Varien_Object))) {
@@ -340,20 +340,20 @@ class BL_CustomGrid_Object extends Varien_Object
                     $data =& $data[$key];
                 }
             }
-            
+
             if (!$found || !isset($data[$lastKey])) {
                 $data[$lastKey] = $default;
             }
-            
+
             return $data[$lastKey];
         }
         return parent::getDataSetDefault($key, $default);
     }
-    
+
     /**
      * Return the value corresponding to the given data key and index if it is considered true,
      * otherwise return the given default value (support compound keys)
-     * 
+     *
      * @param string $key Data key
      * @param mixed $index Data index
      * @param mixed $default Default value
@@ -363,12 +363,12 @@ class BL_CustomGrid_Object extends Varien_Object
     {
         return (($data = $this->getData($key, $index)) ? $data : $default);
     }
-    
+
     /**
      * If a data key is given, return whether a value is set for this key,
      * otherwise return whether this object is not empty
      * (support compound keys)
-     * 
+     *
      * @param string $key Data key (can be compound)
      * @return bool
      */
@@ -379,7 +379,7 @@ class BL_CustomGrid_Object extends Varien_Object
             $keys    =  $this->_explodeCompoundKey($key);
             $lastKey =  array_pop($keys);
             $found   =  true;
-            
+
             foreach ($keys as $key) {
                 if (isset($data[$key])
                     && (is_array($data[$key]) || ($data[$key] instanceof Varien_Object))) {
@@ -393,28 +393,28 @@ class BL_CustomGrid_Object extends Varien_Object
                     break;
                 }
             }
-            
+
             if ($found) {
                 $found = array_key_exists($lastKey, $data);
             }
-            
+
             return $found;
         }
         return parent::hasData($key);
     }
-    
+
     public function __call($method, $args)
     {
         if (substr($method, 0, 2) == 'is') {
             $key = $this->_underscore(substr($method, 2));
             $result = false;
-            
+
             if (array_key_exists('is_' . $key, $this->_data)) {
                 $result = (bool) $this->_data['is_' . $key];
             } elseif (array_key_exists($key, $this->_data)) {
                 $result = (bool) $this->_data[$key];
             }
-            
+
             return $result;
         }
         return parent::__call($method, $args);
